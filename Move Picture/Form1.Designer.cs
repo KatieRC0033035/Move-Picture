b34a466cba0a2ef26c090f01526b427516b35715
Katie Parsonage <rc0033035@student.riversidecollege.ac.uk>
2018-09-11 10:41:19 +0100

    Added up, down, left and right buttons

--- Move Picture/Form1.Designer.cs
+++ Move Picture/Form1.Designer.cs
@@ -29,36 +29,72 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.MoveButton1 = new System.Windows.Forms.Button();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Move_Picture.Properties.Resources.leaf;
-            this.pictureBox1.Location = new System.Drawing.Point(88, 158);
+            this.pictureBox1.Location = new System.Drawing.Point(206, 181);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(90, 72);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // MoveButton1
+            // UpButton
             // 
-            this.MoveButton1.Location = new System.Drawing.Point(546, 206);
-            this.MoveButton1.Name = "MoveButton1";
-            this.MoveButton1.Size = new System.Drawing.Size(110, 24);
-            this.MoveButton1.TabIndex = 1;
-            this.MoveButton1.Text = "Move";
-            this.MoveButton1.UseVisualStyleBackColor = true;
-            this.MoveButton1.Click += new System.EventHandler(this.MoveButton1_Click);
+            this.UpButton.Location = new System.Drawing.Point(580, 123);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(110, 28);
+            this.UpButton.TabIndex = 1;
+            this.UpButton.Text = "Up";
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // DownButton
+            // 
+            this.DownButton.Location = new System.Drawing.Point(580, 191);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(110, 26);
+            this.DownButton.TabIndex = 2;
+            this.DownButton.Text = "Down";
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Location = new System.Drawing.Point(475, 157);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(107, 28);
+            this.LeftButton.TabIndex = 3;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // RightButton
+            // 
+            this.RightButton.Location = new System.Drawing.Point(689, 157);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(99, 27);
+            this.RightButton.TabIndex = 4;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.MoveButton1);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.UpButton);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -70,7 +106,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button MoveButton1;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button RightButton;
     }
 }
 
--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -19,7 +19,27 @@ namespace Move_Picture
 
         private void MoveButton1_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+        }
+
+        private void DownButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
         }
     }
 }
