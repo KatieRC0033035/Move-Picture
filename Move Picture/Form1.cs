b8af4521f574bd888bb13d06e78e4924a9edbd34
Katie Parsonage <rc0033035@student.riversidecollege.ac.uk>
2018-09-11 10:55:17 +0100

    Used // to add comments

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -21,22 +21,22 @@ namespace Move_Picture
         {
             
         }
-
+        //Moves image to the right
         private void RightButton_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 10, pictureBox1.Location.Y);
         }
-
+        //Moves image down
         private void DownButton_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 10);
         }
-
+        //Moves image to the left
         private void LeftButton_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X - 10, pictureBox1.Location.Y);
         }
-
+        //Moves image up
         private void UpButton_Click(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 10);
