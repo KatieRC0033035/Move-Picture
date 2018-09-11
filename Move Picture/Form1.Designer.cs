962c9f1603d3f922c81469c5ad12d95707677ffc
Katie Parsonage <rc0033035@student.riversidecollege.ac.uk>
2018-09-11 10:52:18 +0100

    Started to add diagonal buttons

--- Move Picture/Form1.Designer.cs
+++ Move Picture/Form1.Designer.cs
@@ -33,6 +33,10 @@
             this.LeftButton = new System.Windows.Forms.Button();
             this.RightButton = new System.Windows.Forms.Button();
             this.UpButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +52,7 @@
             // 
             // DownButton
             // 
-            this.DownButton.Location = new System.Drawing.Point(680, 138);
+            this.DownButton.Location = new System.Drawing.Point(680, 129);
             this.DownButton.Name = "DownButton";
             this.DownButton.Size = new System.Drawing.Size(29, 26);
             this.DownButton.TabIndex = 2;
@@ -58,7 +62,7 @@
             // 
             // LeftButton
             // 
-            this.LeftButton.Location = new System.Drawing.Point(645, 115);
+            this.LeftButton.Location = new System.Drawing.Point(653, 104);
             this.LeftButton.Name = "LeftButton";
             this.LeftButton.Size = new System.Drawing.Size(29, 28);
             this.LeftButton.TabIndex = 3;
@@ -68,7 +72,7 @@
             // 
             // RightButton
             // 
-            this.RightButton.Location = new System.Drawing.Point(715, 115);
+            this.RightButton.Location = new System.Drawing.Point(707, 104);
             this.RightButton.Name = "RightButton";
             this.RightButton.Size = new System.Drawing.Size(29, 28);
             this.RightButton.TabIndex = 4;
@@ -78,7 +82,7 @@
             // 
             // UpButton
             // 
-            this.UpButton.Location = new System.Drawing.Point(680, 91);
+            this.UpButton.Location = new System.Drawing.Point(680, 79);
             this.UpButton.Name = "UpButton";
             this.UpButton.Size = new System.Drawing.Size(29, 28);
             this.UpButton.TabIndex = 5;
@@ -86,11 +90,51 @@
             this.UpButton.UseVisualStyleBackColor = true;
             this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(707, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(29, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(653, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(29, 26);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(707, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(29, 26);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.UpButton);
             this.Controls.Add(this.RightButton);
             this.Controls.Add(this.LeftButton);
@@ -110,6 +154,10 @@
         private System.Windows.Forms.Button DownButton;
         private System.Windows.Forms.Button LeftButton;
         private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
